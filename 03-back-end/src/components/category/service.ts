27d4cfc6ce9a569e886a906cda97000e0d9baffa
ognjentr <ogytripkovic@gmail.com>
2021-10-18 23:40:33 +0200

    implements database connection

--- 03-back-end/src/components/category/service.ts
+++ 03-back-end/src/components/category/service.ts
@@ -1,59 +1,100 @@
 import CategoryModel from "./model";
+import * as mysql2 from 'mysql2/promise';
+import IModelAdapterOptions from '../../common/IModelAdapterOptions.interface';
 
 class CategoryService{
+    
+    private db: mysql2.Connection;
+
+    constructor(db: mysql2.Connection){
+        this.db = db;
+    }
+
+    protected async adaptModel (
+        row: any,
+        options: IModelAdapterOptions = { loadParent:false, loadChildren:false}
+        ): Promise<CategoryModel>{
+        const item: CategoryModel =new CategoryModel();
+
+        item.categoryId = +(row?.category_id);
+        item.name = row?.name;
+        item.imagePath = row?.imagePath;
+        item.parentCategoryId = row?.parent_category_id;
+
+        if (options.loadParent && item.parentCategoryId !== null){
+            item.parentCategory = await this.getById(item.parentCategoryId);
+        }
+
+        if(options.loadChildren){
+           item.subcategories  = await this.getAllByParentCategoryId(item.categoryId);
+        }
+        
+        
+        return item;
+    }
+
     public async getAll(): Promise<CategoryModel[]>{
         const lista: CategoryModel[] = [];
 
-        lista.push({
-            categoryId: 1,
-            name: "Category A",
-            imagePath: "static/categories/1.png",
-            parentCategoryId: null,
-            parentCategory: null,
-            subcategories:[],
-        });
+        const sql: string = "SELECT * FROM category WHERE parent_category_id is NULL;";
+        const [rows, colums] = await this.db.execute(sql);
 
-        lista.push({
-            categoryId: 2,
-            name: "Category B",
-            imagePath: "static/categories/2.png",
-            parentCategoryId: null,
-            parentCategory: null,
-            subcategories:[],
-        });
+        if (Array.isArray(rows)){
+            for(const row of rows){
+                lista.push(
+                    await this.adaptModel(
+                        row,{
+    loadChildren: true, 
+    loadParent: false
+}
+                    )
+                )
+            }
+        }    
 
         
         return lista;
     }
+
+    public async getAllByParentCategoryId(parentCategoryId: number): Promise<CategoryModel[]>{
+        const lista: CategoryModel[] = [];
+
+        const sql: string = "SELECT * FROM category WHERE parent_category_id =?;";
+        const [rows, colums] = await this.db.execute(sql,[parentCategoryId]);
+
+        if (Array.isArray(rows)){
+            for(const row of rows){
+                lista.push(
+                    await this.adaptModel(
+                        row,{
+    loadChildren: true, 
+    loadParent: false
+}
+                    )
+                )
+            }
+        }    
+
+        
+        return lista;
+    }
+
     public async getById(categoryId: number): Promise<CategoryModel|null>{
-        if (categoryId === 1|| categoryId === 2 ) {
-            if (categoryId === 1){
-                return{
-                    categoryId: 1,
-            name: "Category A",
-            imagePath: "static/categories/1.png",
-            parentCategoryId: null,
-            parentCategory: null,
-            subcategories:[],
-
-                };
-
-            }
-            if (categoryId === 2){
-             return{
-                categoryId: 2,
-                name: "Category B",
-                imagePath: "static/categories/2.png",
-                parentCategoryId: null,
-                parentCategory: null,
-                subcategories:[],
-
-             };
-
-            }
-        } else{
+        const sql: string = "SELECT * FROM category WHERE category_id =?;";
+        const [rows, colums] = await this.db.execute(sql,[categoryId]);
+        if (!Array.isArray(rows)){
+            return null;
+        }
+        if (rows.length ===0){
             return null;
         } 
+        return await this.adaptModel(rows[0],{
+    loadParent: true,
+    loadChildren: true
+}
+            
+            );
+        
     }
 }
 export default CategoryService;
