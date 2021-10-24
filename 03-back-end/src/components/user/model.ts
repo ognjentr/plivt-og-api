7aa733c93e2097ce43bf095b6b8deab73602886d
ognjentr <ogytripkovic@gmail.com>
2021-10-24 22:26:21 +0200

    Implementing login user logic

--- 03-back-end/src/components/user/model.ts
+++ 03-back-end/src/components/user/model.ts
@@ -1,6 +1,9 @@
 import IModel from "../../common/IModel.interface";
 
 class UserModel implements IModel {
+    passwordHash(password: string, passwordHash: any) {
+        throw new Error('Method not implemented.');
+    }
     userId: number;
     username: string;
     password: string;
