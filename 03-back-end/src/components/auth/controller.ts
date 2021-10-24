dc65c42bacff7d713eb4a164ff0d5e023e385b44
ognjentr <ogytripkovic@gmail.com>
2021-10-24 22:26:34 +0200

    Implementing login user logic

--- 03-back-end/src/components/auth/controller.ts
+++ 03-back-end/src/components/auth/controller.ts
@@ -26,13 +26,13 @@ export default class AuthController extends BaseController {
 
         const authTokenData: ITokenData = {
             id: user.userId,
-            identity: user.email,
+            identity: user.username,
             role: "user",
         };
 
         const refreshTokenData: ITokenData = {
             id: user.userId,
-            identity: user.email,
+            identity: user.username,
             role: "user",
         };
 
@@ -62,73 +62,15 @@ export default class AuthController extends BaseController {
         });
     }
 
-    public async administratorLogin(req: Request, res: Response) {
-        if (!IAdministratorLoginValidator(req.body)) {
-            return res.status(400).send(IAdministratorLoginValidator.errors);
-        }
-
-        const data = req.body as IAdministratorLogin;
-
-        const administrator = await this.services.administratorService.getByUsername(data.username);
-        if (administrator === null) return res.sendStatus(404);
-
-        if (!administrator.isActive) {
-            return res.status(403).send("Administrator account inactive.");
-        }
-
-        if (!bcrypt.compareSync(data.password, administrator.passwordHash)) {
-            // Anti-brute-force mera: sacekati 1s pre slanja odgovora da lozinka nije dobra
-            await new Promise(resolve => setTimeout(resolve, 1000));
-            return res.status(403).send("Invalid administrator password.");
-        }
-
-        const authTokenData: ITokenData = {
-            id: administrator.administratorId,
-            identity: administrator.username,
-            role: "administrator",
-        };
-
-        const refreshTokenData: ITokenData = {
-            id: administrator.administratorId,
-            identity: administrator.username,
-            role: "administrator",
-        };
-
-        const authToken = jwt.sign(
-            authTokenData,
-            Config.auth.administrator.auth.private,
-            {
-                algorithm: Config.auth.administrator.algorithm,
-                issuer: Config.auth.administrator.issuer,
-                expiresIn: Config.auth.administrator.auth.duration,
-            },
-        );
-
-        const refreshToken = jwt.sign(
-            refreshTokenData,
-            Config.auth.administrator.refresh.private,
-            {
-                algorithm: Config.auth.administrator.algorithm,
-                issuer: Config.auth.administrator.issuer,
-                expiresIn: Config.auth.administrator.refresh.duration,
-            },
-        );
-
-        res.send({
-            authToken: authToken,
-            refreshToken: refreshToken,
-        });
-    }
+    
 
     async userRefresh(req: Request, res: Response) {
         this.refreshTokenByRole("user")(req, res);
     }
 
-    async administratorRefresh(req: Request, res: Response) {
-        this.refreshTokenByRole("administrator")(req, res);
-    }
+   
 
-    private refreshTokenByRole(role: "user" | "administrator"): (req: Request, res: Response) => void {
+    private refreshTokenByRole(role: "user" ): (req: Request, res: Response) => void {
         return (req: Request, res: Response) => {
             if (!IRefreshTokenValidator(req.body)) {
                 return res.status(400).send(IRefreshTokenValidator.errors);
